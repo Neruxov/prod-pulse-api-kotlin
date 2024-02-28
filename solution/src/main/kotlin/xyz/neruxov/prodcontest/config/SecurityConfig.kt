8aac1f3e6ae167f769f2f55b774e8dfedf9ff55d
Neruxov <k.neruxov@gmail.com>
2024-02-28 11:40:37 +0300

    merged data

---
+++ solution/src/main/kotlin/xyz/neruxov/prodcontest/config/SecurityConfig.kt
@@ -0,0 +1,12 @@
+package xyz.neruxov.prodcontest.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+/**
+ * @author <a href="https://github.com/Neruxov">Neruxov</a>
+ */
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+}
---
+++ solution/src/main/kotlin/xyz/neruxov/prodcontest/data/user/repo/UserRepository.kt
@@ -0,0 +1,7 @@
+package xyz.neruxov.prodcontest.data.user.repo
+
+/**
+ * @author <a href="https://github.com/Neruxov">Neruxov</a>
+ */
+interface UserRepository {
+}
